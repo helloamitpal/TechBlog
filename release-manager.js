e5405f88a080911c4961df0ee46e05b22ea168f5
IN22913318 <amit.pal@tesco.com>
2020-05-17 12:41:41 +0530

    application version is updated

--- release-manager.js
+++ release-manager.js
@@ -1,15 +1,19 @@
 const cliSelect = require('cli-select');
 const chalk = require('chalk');
+const { exec } = require('child_process');
 
 cliSelect({
   values: ['Major', 'Minor', 'Patch'],
-  valueRenderer: (value, selected) => {
+  valueRenderer: function (value, selected) {
     if (selected) {
       return chalk.underline(value);
     }
 
     return value;
   }
-}).then(({ value }) => {
-  exec(`npm version ${value.toLowerCase()} --force -m "application version is updated"`);
+}).then(function(resp) {
+  console.log(resp.value + 'release is selected');
+  exec('npm version '+resp.value.toLowerCase()+' --force -m "application version is updated"');
+}, function() {
+  console.log('Failed to execute');
 });
