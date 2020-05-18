5c673a3915118386cecb7164651474111e341c9a
IN22913318 <amit.pal@tesco.com>
2020-05-18 08:28:55 +0530

    refactor: 💡 Updated release manager code

--- release-manager.js
+++ release-manager.js
@@ -13,7 +13,12 @@ cliSelect({
   }
 }).then(function(resp) {
   console.log(chalk.green(resp.value + ' release is selected'));
-  exec('npm version '+resp.value.toLowerCase()+' --force --no-git-tag-version && git add package.json package-lock.json');
+  const scripts = [
+    'npm version '+resp.value.toLowerCase()+' --force --no-git-tag-version',
+    'git add package.json package-lock.json',
+    'git commit -m "Updated application '+ resp.value +' version"'
+  ];
+  exec(scripts.join(' & '));
 }, function() {
   console.log(chalk.red('Failed to update application version'));
 });
